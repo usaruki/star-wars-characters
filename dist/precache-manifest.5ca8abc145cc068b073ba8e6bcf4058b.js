183996137b15ff341d58208f52665a57f3b2a908
Henrique Melo <henrique@pampadevs.com>
2022-01-19 10:28:19 -0300

    dist folder

---
+++ dist/precache-manifest.5ca8abc145cc068b073ba8e6bcf4058b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2981c58b7d30644ad623",
+    "url": "/css/app.8f6032e7.css"
+  },
+  {
+    "revision": "75c0119ca475dea587b8661501320be5",
+    "url": "/fonts/SF Distant Galaxy.75c0119c.ttf"
+  },
+  {
+    "revision": "09637623bbeef31e019f163f04386959",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2981c58b7d30644ad623",
+    "url": "/js/app.d9ded36e.js"
+  },
+  {
+    "revision": "0f187cfc1ef3b34da04a",
+    "url": "/js/chunk-vendors.104e8813.js"
+  },
+  {
+    "revision": "e903b139df34f9492043d41bfb7751a7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
